804cd5068ed198ec4c7b96c7307785a2688762d6
Jose Dimas Carabantes Garcia <dimasgarcia@Joses-MBP.domain.name>
2021-03-21 16:08:42 -0600

    Primer Commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,59 @@
+
+const fs = require('fs');
+const colors = require('colors');
+
+/*const crearArchivo = ( base = 5 ) => {
+
+    return new Promise((resolve, reject) => {
+
+        console.log('================');
+        console.log(' Tabla del:', base);
+        console.log('================');
+    
+        let salida = '';
+    
+        for  (let i =1; i <=10; i++){
+            salida += `${ base } x ${ i } = ${ base * i}\n`;
+        }
+    
+        console.log(salida);
+    
+        fs.writeFileSync(`tabla-${ base }.txt`, salida);
+    
+        resolve(`tabla-${ base }.txt`);
+
+    });
+}*/
+
+// using async 
+const crearArchivo = async( base = 5, listar = false, hasta = 10 ) => {
+
+    try{
+
+        let salida = '';
+        let temp = '';
+    
+        for  (let i =1; i <= hasta; i++){
+            salida += `${ base } x ${ i } = ${ base * i}\n`;
+            temp += `${ base } ${ 'x'.green} ${ i } ${ '='.green } ${ base * i}\n`;
+        }
+
+        if ( listar ) {
+            console.log('================'.green);
+            console.log(' Tabla del:'.green, colors.blue( base ));
+            console.log('================'.green);
+            console.log(temp);
+        }
+    
+        fs.writeFileSync(`./salida/tabla-${ base }.txt`, salida);
+    
+        return `tabla-${ base }.txt`;
+    }catch( err ) {
+        throw err;
+    }
+}
+
+module.exports = {
+    crearArchivo
+}   
+
